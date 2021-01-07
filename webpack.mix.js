8468866a301b5ea53973eab4610938c457a0a429
jjs9000 <farriszamrie15@gmail.com>
2021-01-07 22:17:34 +0800

    first initial push

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,7 @@ mix.js('resources/js/app.js', 'public/js')
 
     mix.browserSync({
         proxy: {
-            target: "http://template.test/",
+            target: "http://messenger.test/",
             ws: true
         },
         port: 8000,
